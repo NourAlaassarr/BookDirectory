9560ad0d50c2d070954158fafe9500fb56269550
itsalaassar <itsalaassar@gmail.com>
2023-09-24 18:33:07 +0300

    beginning

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from'express'
+
+const app=express()
+const Port =3000
+app.listen(Port,()=>{
+    console.log(`---------------Server is running on port Number ${Port}!----------------------`)
+})
