9f180c3f9c0972eb408ddd6ffbb84d9001ee1392
itsalaassar <itsalaassar@gmail.com>
2023-09-29 16:00:49 +0300

    connection with Mongo Atlas

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from'express'
 import { InitiateApp } from './src/utlis/initiateApp.js'
 import {config} from'dotenv'
 import path from 'path'
+
 config({ path: path.resolve('./config/config.env') })
 
 
--- src/utlis/initiateApp.js
+++ src/utlis/initiateApp.js
@@ -1,13 +1,16 @@
-
-
-
+import { DBConnection } from "../../DB/Connection.js"
+import cors from 'cors'
 
 
 export const InitiateApp=(App,express)=>{
     const Port=process.env.PORT ||5000
     App.use(express.json()) 
-
-
+    DBConnection()
+    
+    //cors
+    App.use(cors()) // allow anyone
+    App.get('./',(req,res)=>res.send("Hello World!"))
+    App.all('*',(req,res,next)=>res.status(404).json({message:'URL NOT FOUND.'}))
     App.listen(Port,()=>{
         console.log(`---------------Server is Running on port number ${Port} !---------------`)
     })
